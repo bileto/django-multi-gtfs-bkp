5374831b0cd2dabed8a4cdbafee9177a3957bb6f
Dalibor Pavlovic <dalibor.pavlovic@bileto.com>
2023-08-13 22:56:38 +0200

    Upgrade to Django 4.2

--- examples/explore/exploreproj/urls.py
+++ examples/explore/exploreproj/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import include, url
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name="explore/home.html"),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', TemplateView.as_view(template_name="explore/home.html"),
         name='home'),
-    url(r'', include('exploreapp.urls'))
+    path('', include('exploreapp.urls'))
 ]
